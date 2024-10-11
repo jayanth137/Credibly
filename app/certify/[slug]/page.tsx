294390fd9816b1b01de14ac5a68699349303ee1b
Kaushik Saha <kaushiksaha004@gmail.com>
2024-10-11 19:20:10 +0530

    loader

--- app/certify/[slug]/page.tsx
+++ app/certify/[slug]/page.tsx
@@ -1,9 +1,10 @@
 'use client'
 import { FC, useEffect, useState } from 'react';
 import { Button } from "@/components/ui/button"
-import { Wallet, Play, Copy, User, CheckCircle, Check } from "lucide-react"
+import { Wallet, Play, Copy, User, CheckCircle, Check, Loader } from "lucide-react"
 import PriceInfo from '@/app/components/User/coursePage/PriceInfo';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import Lottie from 'react-lottie'
 
 interface Params {
     slug: string;
@@ -87,12 +88,23 @@ const SlugPage: FC<{ params: Params }> = ({ params }) => {
         return <p>Error: {error}</p>
     }
     if (!data) {
-        return <p>Loading...</p>
+
+        return <div className='h-screen w-full flex items-center justify-center text-white flex-col gap-5 text-center'>
+            <Lottie options={{
+                animationData: require('@/public/Loader.json'),
+                loop: true,
+                autoplay: true,
+            }}
+                height={200}
+                width={200}
+            />
+            <h1 className='font-bold text-xl'>Awesome content is Loading...</h1>
+        </div>
     }
     return (
         <div className="min-h-screen text-white">
             <main className="mx-auto px-4 py-8 md:py-12 w-10/12 ">
-                <div className=" p-6 rounded-lg w-full mx-auto">
+                <div className=" p-6 rounded-lg w-8/12 mx-auto">
 
                     <div className="flex flex-col md:flex-row justify-evenly ">
                         <div className="space-y-6">
@@ -103,16 +115,9 @@ const SlugPage: FC<{ params: Params }> = ({ params }) => {
                                     className="w-full h-full object-cover"
                                 />
                                 <div className="absolute inset-0 flex items-center justify-center">
-                                    {/* <Button variant="outline" size="icon" className="rounded-full bg-white/10 hover:bg-white/20">
-                                        <Play className="h-6 w-6" />
-                                    </Button> */}
                                 </div>
                             </div>
 
-                            {/* <div className="flex items-center space-x-2">
-                                <User className="w-5 h-5 text-gray-400" />
-                                <span className="font-semibold">Author: {data.author}</span>
-                            </div> */}
                             <div className='space-y-4'>
                                 <h1 className="text-2xl font-bold text-white mb-4">
                                     {data.title}
@@ -120,13 +125,6 @@ const SlugPage: FC<{ params: Params }> = ({ params }) => {
                                 <div className="flex items-center justify-between ">
                                     <div>
                                         <div className=" flex items-center justify-center backdrop-blur-lg bg-white/10 rounded-lg border border-white/20 shadow-lg p-2 px-3 ">
-                                            <Avatar className="w-12 h-12">
-                                                <AvatarImage
-                                                    src="https://github.com/shadcn.png"
-                                                    alt="@shadcn"
-                                                />
-                                                <AvatarFallback>CN</AvatarFallback>
-                                            </Avatar>{' '}
                                             <p className="text-white">
                                                 Total enrolled : <span>40</span>
                                             </p>
@@ -135,8 +133,9 @@ const SlugPage: FC<{ params: Params }> = ({ params }) => {
 
                                     <div className=" flex backdrop-blur-lg gap-4 bg-white/10 rounded-lg border border-white/20 shadow-lg p-2">
                                         <Avatar className="w-12 h-12">
-                                            <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+                                            <AvatarImage src="https://thumbs.dreamstime.com/b/default-avatar-profile-icon-vector-social-media-user-photo-concept-285140929.jpg" alt="@shadcn" />
                                             <AvatarFallback>CN</AvatarFallback>
+
                                         </Avatar>{' '}
                                         <div className='flex items-center'>
                                             <p className="text-white">{data.author}</p>
@@ -152,11 +151,15 @@ const SlugPage: FC<{ params: Params }> = ({ params }) => {
                                 <CheckCircle className="w-5 h-5 text-green-400" />
                                 <span className="font-semibold">Validation Type: {data.validation}</span>
                             </div>
+                            <div className="flex w-full justify-center items-center space-x-2">
+                                <Button className='bg-white text-black font-bold hover:bg-gray-400'>Claim Certificate {'(0.002 ETH)'}</Button>
+                            </div>
+
                         </div>
 
-                        <div className="space-y-6">
+                        {/* <div className="space-y-6">
                             <PriceInfo validation={data.validation} />
-                        </div>
+                        </div> */}
                     </div>
                 </div>
             </main>
@@ -168,3 +171,6 @@ const SlugPage: FC<{ params: Params }> = ({ params }) => {
 };
 
 export default SlugPage;
+
+
+
