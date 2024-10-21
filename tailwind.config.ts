1497d0bae58dac61f6503742746384bcd3db00f8
Jayanth <jayanth.sms.in@gmail.com>
2024-10-21 05:31:18 +0530

    hypothesis 3

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,26 @@ const config: Config = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
 
+  theme: {
+   
+    container: {
+      center: true,
+      padding: '2rem',
+      screens: {
+        '2xl': '1400px',
+      },
+    },
+    screens: {
+      sm: '360px',
+
+      md: '720px',
+
+      lg: '1024px',
+
+      xl: '1280px',
+    },
+  },
+
   plugins: [require('tailwindcss-animate')],
 };
 export default config;
